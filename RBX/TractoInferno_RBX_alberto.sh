4fffbb286575cb6f95c4ab41d18b9b6789be0c9a
Spolverato Alberto (ACC0128525) <alberto.spolverato@unitn.it@irbio-server1.cimec.unitn.it>
2024-07-29 16:44:34 +0200

    reduced batches processed

--- RBX/TractoInferno_RBX_alberto.sh
+++ RBX/TractoInferno_RBX_alberto.sh
@@ -18,17 +18,17 @@ execute_and_print() {
 }
 
 
-atlas_rbx_path="/home/alberto.spolverato/mnt/atlas_RBX/atlas_RBX/atlas_v1.1/atlas_v1.1"
-atlas_anat_mni="/home/alberto.spolverato/mnt/atlas_RBX/atlas_RBX/atlas_v1.1/mni_masked.nii.gz"
-config_path="/home/alberto.spolverato/mnt/atlas_RBX/atlas_RBX/atlas_v1.1/config_v2.2/config_ind_copy.json"
+atlas_rbx_path="/home/alberto.spolverato/mnt/myDataset/atlas_RBX/atlas_v1.1/atlas_v1.1"
+atlas_anat_mni="/home/alberto.spolverato/mnt/myDataset/atlas_RBX/atlas_v1.1/mni_masked.nii.gz"
+config_path="mnt/myDataset/atlas_RBX/atlas_v1.1/config_v2.2/config_ind_copy.json"
 rbx_main_path="/home/alberto.spolverato/local/rbx_flow/main.nf"
 container_scilus_path="/home/alberto.spolverato/mnt/container_scilus/scilus_1.5.0.sif"
 
-vector=("1" "2") 
+vector=("3" "4" "5" "6" "7" "8" "9" "10" "11" "12" "13" "14" "15" "16" "17" "18" "19" "20" "21" "22" "23" "24" "25" "26") 
 for i in "${vector[@]}"
 
 do
-    dataset_path="/home/alberto.spolverato/mnt/TractoInferno/derivatives/input_rbX/batch${i}"
+    dataset_path="/home/alberto.spolverato/mnt/myDataset/TractoInferno/derivatives/input_rbX/batch${i}"
     #out_dir="/home/chiara.riccardi-1/20240613_test_RBX_fr/"
     out_dir="/home/alberto.spolverato/mnt/Experiments/batch${i}"
 
@@ -41,13 +41,11 @@ do
 
     echo 'start' >> $log_file
 
-    nextflow_cmd="nextflow run ${rbx_main_path} -with-singularity ${container_scilus_path} -resume --input ${dataset_path} --atlas_directory ${atlas_rbx_path} --atlas_config ${config_path} --atlas_anat ${atlas_anat_mni} --rbx_processes 10  --memory '110 GB' &>> ${log_file}"
+    nextflow_cmd="nextflow run ${rbx_main_path} -with-singularity ${container_scilus_path} -resume --input ${dataset_path} --atlas_directory ${atlas_rbx_path} --atlas_config ${config_path} --atlas_anat ${atlas_anat_mni} --rbx_processes 10 --processes 1 -profile large_dataset,cbrain &> ${log_file}"
 
     execute_and_print "${nextflow_cmd}"
     echo 'end' >> $log_file
     
     pwd
-
-    cp -Lr results_rbx/ results_rbx_noL/
 
 done
